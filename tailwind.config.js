47e4ef7a3930e986c42c632312322deaeaa05322
João Marques <joaopmarques@users.noreply.github.com>
2021-08-27 08:15:49 +0100

    Style settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': 'IBM Plex Sans, sans-serif'
+    },
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+      '400': '400px'
+     },
+     borderRadius: {
+       '4xl': '2rem',
+       '5xl': '2.5rem',
+       '6xl': '3rem'
+     }
   },
   variants: {
     extend: {},
