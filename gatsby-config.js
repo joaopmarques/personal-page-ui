dd7f77bb3fccde3e32ae2327ffc942956bdc3edf
João Marques <joaopmarques@users.noreply.github.com>
2021-09-02 06:59:40 +0100

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,6 @@ module.exports = {
       __key: "pages",
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
-    {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `https://jpmarques-website.herokuapp.com`,
